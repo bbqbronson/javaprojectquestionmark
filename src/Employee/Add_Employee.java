c1fd0e534028a45cf279aca894d8bde678753a40
Bronson Perez <bperez1081@gmail.com>
2021-08-19 23:15:05 -0700

    add_employee

--- src/Employee/Add_Employee.java
+++ src/Employee/Add_Employee.java
@@ -7,5 +7,10 @@ import java.io.*;
 import javax.swing.*;
 
 class Add_Employee implements ActionListener{
-  
+JFrame f;
+    JLabel id,id1,id2,id3,id4,id5,id6,id7,id8,id9,id10,id11,id12,id15,id16,id17,lab,lab1;
+    JTextField t,t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
+    JButton b,b1,b2,b3;
+
+    
 }
